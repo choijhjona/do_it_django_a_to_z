1de93f1a46b8e03d7c83a70cb061e7c53952a71d
박응용 <choijhjona>
2023-04-15 15:33:07 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,11 @@ def index(request):
         }
     )
 
+def single_post_page(request.pk):
+    post = Post.objects.get(pk=pk)
 
+    return render(
+        request,
+        blog/single_post_page.html',
+{}
+    )
